11ef77ff89b9ce1e74fb05e60aa333fa4764fc7b
Wang Quốc Khánh <wangquockhanh@MacBook-Air-cua-Wang.local>
2024-11-13 10:38:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+{const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  if (input === 0) {
+    result.innerText = "0";
+    return;
+  }
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");}
